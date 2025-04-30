84674db6cb7a2513c5698863752ad678ba8ce202
Arif Akbarudin <arifakbarudin7@gmail.com>
2025-04-30 07:19:13 +0700

    Add files via upload
    tugas tampilan flutter widget login - profil

---
+++ tugas/login&bagraund/widget_login_profil/componen/card_produk.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:login_profil_widget/componen/button.dart';
+import 'package:login_profil_widget/componen/rating_produk.dart';
+
+class CardProduk extends StatelessWidget {
+  const CardProduk({
+    super.key,
+    required this.namaProduk,
+    required this.harga,
+    required this.imagePath,
+    this.stok,
+    this.warna,
+  });
+
+  final String namaProduk;
+  final double harga;
+  final int? stok;
+  final Color? warna;
+  final dynamic imagePath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        color: warna ?? const Color.fromARGB(255, 255, 255, 255),
+        height: 300,
+        width: 200,
+        child: Padding(
+          padding: const EdgeInsets.all(19.0),
+          child: Column(
+            children: [
+              CircleAvatar(radius: 45, backgroundImage: AssetImage(imagePath)),
+              const SizedBox(height: 10),
+              Text(
+                "$namaProduk",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                ),
+              ),
+              const SizedBox(height: 4),
+              Text(
+                "Harga : $harga",
+                style: TextStyle(
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Text(
+                "Stok : ${stok ?? 0}",
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+              ),
+              RatingProduk(), // Jika ada widget RatingProduk
+              WidgetButton(
+                label: Row(
+                  mainAxisSize: MainAxisSize.min, // agar posisinya tertata
+                  children: [
+                    Icon(Icons.shopping_cart),
+                    SizedBox(width: 8),
+                    Text("Beli"),
+                  ],
+                ),
+                onPressed: () {
+                  // Aksi kalau tombol "Beli" ditekan
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text("Berhasil membeli $namaProduk")),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
